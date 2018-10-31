cd29207c1478753809ba7fa51e7e9efba9451d18
xuxiaoyin <279238850@qq.com>
2018-10-31 15:50:40 +0800

    change

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,18 +5,30 @@
 		<ul>
 			<li v-for="item in list">{{item.TITLE}}</li>
 		</ul>
+		<br />
+		<br />
+		<br />
+		<hr />
+		<br />
+		<br />
+		<br />
+		<button @click="emitNews()">非父子组件Home</button>
+		<div>{{info}}</div>
+		
 	</div>
 </template>
 
 <script>
 	import Header from './Header.vue'
+	import VueEvent from '../model/VueEvent.js'
 	export default{
 		data(){
 			return{
 				list:[],
 				index:1,
 				totalPage:'',
-				msg:'我是Home组件'
+				msg:'我是Home组件',
+				info:'Home组件的值'
 			}
 		},
 		components:{
@@ -40,10 +52,16 @@
 					}
 					console.log(this.list)
 				},err=>{console.log(err)})
+			},
+			emitNews(){
+				VueEvent.$emit('to-news',this.info);
 			}
 		},
 		mounted(){
 			this.getData();
+			VueEvent.$on('to-home',data=>{
+				this.info=data;
+			})
 		}
 	}
 </script>
--- src/components/News.vue
+++ src/components/News.vue
@@ -1,5 +1,15 @@
 <template>
 	<div>
+		<br />
+		<br />
+		<br />
+		<hr />
+		<br />
+		<br />
+		<br />
+		<button @click="emitHome">非父子组件News</button>
+		<div>{{info}}</div>
+		
 		<h1>{{msg}}</h1>
 		<button @click="change()">改变父组件</button>
 		<v-foot ref='footer'></v-foot>
@@ -8,11 +18,14 @@
 
 <script>
 	import Footer from './Footer.vue'
+	import VueEvent from '../model/VueEvent.js'
 	export default{
 		data(){
 			return{
 				msg:'父组件',
-				msg1:'我是父组件里面的值'
+				msg1:'我是父组件里面的值',
+				info:'News组件的值',
+				info1:'News组件的值'
 			}
 		},
 		components:{
@@ -23,9 +36,18 @@
 				alert('父组件的方法')
 			},
 			change(){
-				this.msg=this.$refs.footer.msg1
+				this.msg=this.$refs.footer.msg1;
 				this.$refs.footer.run();
+			},
+			emitHome(){
+				VueEvent.$emit('to-home',this.info1);
 			}
+		},
+		mounted(){
+			VueEvent.$on('to-news',data=>{
+				console.log(data)
+				this.info=data;
+			})
 		}
 	}
 </script>
